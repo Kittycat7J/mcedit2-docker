4b1213c8ec81c2d36a8e688ff05769a1b1f7f915
Kittycat7J <167596510+Kittycat7J@users.noreply.github.com>
2025-04-18 08:10:35 -0400

    add working script

--- docker_test/runMCEdit.sh
+++ docker_test/runMCEdit.sh
@@ -1,2 +1,2 @@
 docker build . -f mcedit -t mcedit2
-# docker run --rm -it --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro mcedit2
+docker run --rm -it --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro mcedit2
